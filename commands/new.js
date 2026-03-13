ddedf5cad26e0a95e43aed148c4367982a5d0b87
Mebiasoft <admin@mebiasoft.com>
2026-03-14 00:56:56 +0300

    add interactive project wizard

--- commands/new.js
+++ commands/new.js
@@ -1,5 +1,6 @@
 import CoreEngine from "../core/engine.js";
 import LaravelModule from "../modules/laravel/index.js";
+import { runWizard } from "../core/wizard.js";
 
 export default function newCommand(program) {
 
@@ -8,11 +9,11 @@ export default function newCommand(program) {
     .description("create a new laravel project")
     .action(async (project) => {
 
-      console.log(`Creating new project: ${project}`);
+      const config = await runWizard(project);
 
       const engine = new CoreEngine();
 
-      const laravelModule = new LaravelModule(project);
+      const laravelModule = new LaravelModule(config.projectName);
 
       engine.register(laravelModule);
 
