b2298571045ced6f407bcd24133121658c38b65a
Mebiasoft <admin@mebiasoft.com>
2026-03-13 23:33:29 +0300

    create commands directory for CLI structure

--- bin/larafast.js
+++ bin/larafast.js
@@ -1,3 +1,44 @@
 #!/usr/bin/env node
 
-console.log("Larafast CLI çalışıyor 🚀");
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("larafast")
+  .description("Laravel project installer CLI")
+  .version("1.0.0");
+
+program
+  .command("new <project>")
+  .description("create a new laravel project")
+  .action((project) => {
+    console.log(`Creating new project: ${project}`);
+  });
+
+program
+  .command("doctor")
+  .description("check development environment")
+  .action(() => {
+    console.log("Checking environment...");
+  });
+
+program
+  .command("install <module>")
+  .description("install module into laravel project")
+  .action((module) => {
+    console.log(`Installing module: ${module}`);
+  });
+
+program
+  .command("list")
+  .description("list available modules")
+  .action(() => {
+    console.log("Available modules:");
+    console.log("- breeze");
+    console.log("- jetstream");
+    console.log("- tailwind");
+    console.log("- filament");
+  });
+
+program.parse();
